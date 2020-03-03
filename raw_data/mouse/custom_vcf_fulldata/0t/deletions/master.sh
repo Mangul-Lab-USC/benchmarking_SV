53babce70f81d17b5f597fccb366d0545e923250
alu52904 <angelalu@Angelas-MacBook-Pro.local>
2020-03-03 13:29:31 -0800

    updated specificity

---
+++ raw_data/mouse/custom_vcf_fulldata/0t/deletions/master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -fr nf_*
+ls *vcf >samples.txt
+
+while read line
+do
+
+#echo $line
+cat ../../../../../scripts/header_vcf.txt >nf_${line}
+sed 's/FP/;FLAG=FP/' ${line} | grep FP | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8$9}' >>nf_${line} 
+sed 's/TP/;FLAG=TP/' ${line} | grep TP | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8$9}' >>nf_${line} 
+sed 's/TN/;FLAG=TN/' ${line} | grep TN | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8$9}' >>nf_${line}
+
+done<samples.txt
